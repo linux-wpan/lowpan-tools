836c5001c05983732f7e0db2ab03cd0176043a20
Sergey Lapin <slapin@slind.org>
2009-01-14 01:30:57 +0300

    Proper serial setup
    Signed-off-by: Sergey Lapin <slapin@slind.org>

--- serial.c
+++ serial.c
@@ -9,18 +9,21 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <linux/if.h>
 
 #include "ieee802154.h"
 #define N_ZB 19
 
 int main(int argc, char **argv) {
-	int fd, ret;
+	int fd, ret, s;
+	struct ifreq req;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s SERIAL_DEV\n", argv[0]);
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s SERIAL_DEV master_dev\n", argv[0]);
 		return 1;
 	}
 
@@ -35,11 +38,19 @@ int main(int argc, char **argv) {
 	memset(&tbuf, 0, sizeof(tbuf));
 
 	tbuf.c_iflag |= IGNBRK;		/* Ignores break condition */
-	tbuf.c_iflag |= INPCK;		/* Enable input parity check */
+	tbuf.c_iflag &= !(INPCK|ICANON|ECHO);		/* */
+	tbuf.c_cc[VMIN] = 1;
 
 	tbuf.c_cflag |= CS8;		/* 8 bits */
 	tbuf.c_cflag |= CREAD;		/* Enables receiver */
-	tbuf.c_cflag |= CRTSCTS;	/* not in POSIX. Enable RTS/CTS (hardware) flow control. requires _BSD_SOURCE or _SVID_SOURCE */
+	tbuf.c_cflag |= CLOCAL;		/*  */
+	tbuf.c_cflag &= ~CRTSCTS;	/* not in POSIX. Enable RTS/CTS (hardware) flow control. requires _BSD_SOURCE or _SVID_SOURCE */
+        tbuf.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
+                          | INLCR | IGNCR | ICRNL | IXON);
+        tbuf.c_oflag &= ~OPOST;
+        tbuf.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+        tbuf.c_cflag &= ~(CSIZE | PARENB);
+                                                                                  
 	/*
 	tbuf.c_cflag |= CLOCAL;
 	tbuf.c_lflag = 0;
@@ -65,11 +76,13 @@ int main(int argc, char **argv) {
 		perror("tcgetattr");
 		return 4;
 	}
+#if 0
+	/* WTF?? */
 	if (0 == (tbuf.c_cflag & CRTSCTS)) {
 		fprintf(stderr, "failed to set CRTSCTS\n");
 		return 5;
 	}
-
+#endif
 	int arg;
 	arg = N_IEEE80215;
 	ret = ioctl(fd, TIOCSETD, &arg);
@@ -77,6 +90,21 @@ int main(int argc, char **argv) {
 		perror("ioctl: TIOCSETD");
 		return 6;
 	}
+
+	s = socket(PF_IEEE80215, SOCK_RAW, 0);
+	if (ret < 0) {
+		perror("socket");
+		return 7;
+	}
+	strcpy(req.ifr_name, argv[2]);
+	memset(&req.ifr_hwaddr.sa_data, 0, sizeof(req.ifr_hwaddr.sa_data));
+	strcpy(req.ifr_hwaddr.sa_data, argv[2]);
+	ret = ioctl(s, IEEE80215_SIOC_ADD_SLAVE, &req);
+	if (ret < 0) {
+		perror("ioctl: IEEE80215_SIOC_ADD_SLAVE");
+		return 8;
+	}
+	close(s);
 
 	while (1);
 
