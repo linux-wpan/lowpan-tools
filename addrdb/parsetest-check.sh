6950a70591dba5b61a874e3c89d1fe1e1abf7e3d
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-09-10 01:57:40 +0400

    addrdb: simplify checking script a bit
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- addrdb/parsetest-check.sh
+++ addrdb/parsetest-check.sh
@@ -4,7 +4,7 @@ if [ -z "$LEASE_DIR" ]; then
 	LEASE_DIR="/var/tmp"
 fi
 mkdir -p ${LEASE_DIR}
+echo > ${LEASE_DIR}/izcoordinator.leases
 LEASE_FILE=${LEASE_DIR}/izcoordinator.leases
-./parsetest ${LEASE_FILE} || exit 1
-exit 0
+exec ./parsetest ${LEASE_FILE}
 
